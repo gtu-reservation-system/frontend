4cdecea798886de69568e15deef8a5011a74a360
yusufdinc974 <dinc.yusuf974@gmail.com>
2025-01-17 14:47:16 +0300

    login navbar updated

--- src/components/LoginNavbar.js
+++ src/components/LoginNavbar.js
@@ -6,15 +6,10 @@ import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const LoginNavbar = () => {
   const navigate = useNavigate();
-  const [showDropdown, setShowDropdown] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
-  const toggleDropdown = () => {
-    setShowDropdown((prev) => !prev);
   };
 
   const NavigationItems = () => (
@@ -22,47 +17,34 @@ const LoginNavbar = () => {
       <button className="nav-button" onClick={() => navigate('/home')}>
         Ana Sayfa
       </button>
-
-      <div className="signup-container">
-        <button className="signup-button" onClick={toggleDropdown}>
-          Kayıt Ol
-        </button>
-        {showDropdown && (
-          <div className="dropdown">
-            <button
-              className="dropdown-item"
-              onClick={() => {
-                navigate('/signup/owner');
-                setIsMobileMenuOpen(false);
-                setShowDropdown(false);
-              }}
-            >
-              Restoran
-            </button>
-            <button
-              className="dropdown-item"
-              onClick={() => {
-                navigate('/signup/user');
-                setIsMobileMenuOpen(false);
-                setShowDropdown(false);
-              }}
-            >
-              Kullanıcı
-            </button>
-          </div>
-        )}
-      </div>
+      <button 
+        className="signup-button"
+        onClick={() => {
+          navigate('/signup/owner');
+          setIsMobileMenuOpen(false);
+        }}
+      >
+        Restoran Kayıt
+      </button>
+      <button 
+        className="signup-button"
+        onClick={() => {
+          navigate('/signup/user');
+          setIsMobileMenuOpen(false);
+        }}
+      >
+        Kullanıcı Kayıt
+      </button>
     </>
   );
 
   return (
     <div className="home-navbar">
       <div className="navbar-main">
-        <div className="logo" onClick={() => navigate('/')}> 
+        <div className="logo" onClick={() => navigate('/')}>
           <img src="/icon.png" alt="Logo" />
           Rezerve
         </div>
-
         <button
           className="mobile-menu-button"
           onClick={toggleMobileMenu}
@@ -71,12 +53,10 @@ const LoginNavbar = () => {
           <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
         </button>
       </div>
-
       {/* Desktop Navigation */}
       <nav className="navbar desktop-nav">
         <NavigationItems />
       </nav>
-
       {/* Mobile Navigation */}
       <nav className={`mobile-nav ${isMobileMenuOpen ? 'open' : ''}`}>
         <NavigationItems />
